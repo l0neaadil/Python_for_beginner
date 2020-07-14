2f1048de17ef213941ce39818e182df89bc8f52c
l0neaadil <loneaadil008@outlook.com>
2020-07-14 21:06:37 +0530

    input function

---
+++ 02_input.py
@@ -0,0 +1,8 @@
+# Input
+Name = input("what is ur name   ")
+print(type(Name))
+Residence = input("Your residence is at :   ")
+print("So ur name is", Name, ' and u r from  ', Residence)
+DOB = int(input("now tell ur DOB  "))
+print(type(DOB))
+print('your age is ', 2020 - DOB)
