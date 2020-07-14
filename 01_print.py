d45b50d17431d3e21eb158a520752859e19c9527
l0neaadil <loneaadil008@outlook.com>
2020-07-14 20:53:02 +0530

    all about print

---
+++ 01_print.py
@@ -0,0 +1,20 @@
+# PRINT
+print("Lone_Aadil")
+print(14203135008)
+print("x=10", "y=11")
+print("x=10 " + "y=11")
+print('x=', 10)
+print("y", 12)
+print(1, 2, 3, 4, 5)
+print(1, 2, "e")
+z = "a"
+y = 6
+x = 7
+print(z)
+print(y)
+print(x, y, z)
+print("sum = %d" % (x+y))
+print('p=%d, s=%d, d=%d' % ((x*y), (x+y), (x-y)))
+print(f'p={x*y}, s={x+y}, d={x-y}')
+print('*' * 10)
+
