a952f143bcd4cf3337115e53b6a4700ce6fbfd46
l0neaadil <loneaadil008@outlook.com>
2020-07-17 20:46:14 +0530

    set and operations on a set

---
+++ 07_set.py
@@ -0,0 +1,26 @@
+# Sets
+# a set contains unique elements only
+
+a = {1, 9, 4, 5, 8, 1, 2, 9}
+print(a, '\n', len(a))
+
+a.add(15)
+a.update([3, 25, 27, 29])
+print(a)
+
+a.remove(25)                     # if element is not present it will show error
+a.discard(27)                    # if element is not present it will not show error
+a.pop()
+print(a)
+
+a.clear()
+print(a)
+
+a = {1, 2, 3, 4, 5}
+b = {5, 4, 6, 7, 8, 9}
+c = a | b                                # union of a and b
+d = a.union(b)                           # same as a | b
+e = a & b                                # intersection of a and b
+f = a.intersection(b)                    # same as a & b
+print(a, b, '\n', c, d, '\n', e, f)
+
