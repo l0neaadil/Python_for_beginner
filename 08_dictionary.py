d26d088baf00762d5d26fa8f8f137483fc12cd7d
l0neaadil <loneaadil008@outlook.com>
2021-02-06 15:05:01 +0530

    dictionary

--- 08_dictionary.py
+++ 08_dictionary.py
@@ -17,6 +17,6 @@ print(len(f), len(g))
 g[1] = 67                                 # replacing value of key
 g[5] = 'y'                                  # adding new key_value pair
 print(g)
-print(f.keys(), "\n", g.values())
+print(f.keys(), "\n", g.values(), '\n' , f.items())
 for el, v in f.items():                     # el represents keys & v represents values
     print(el, v)
