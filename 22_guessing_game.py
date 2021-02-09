36ead11da9ee17020152486d5f97cbe4d2b09c90
l0neaadil <loneaadil008@outlook.com>
2021-02-09 17:04:07 +0530

    break

--- 22_guessing_game.py
+++ 22_guessing_game.py
@@ -1,17 +1,17 @@
 # Guessing Game
-Secret_No = 7
-Guess_No = 0
-Guess_Limit = 3
-Attempts = 3
-while Guess_No < Guess_Limit:
-    Guess = int(input("Guess a No... "))
-    Guess_No += 1
-    if Guess == Secret_No:
-        print('Your guess is right.You are a genius.')
+secret_No = 7
+guess_No = 0
+guess_Limit = 3
+attempts = 3
+while guess_No < guess_Limit:
+    guess = int(input("Guess a No...> "))
+    guess_No += 1
+    if guess == secret_No:
+        print('Your guess is right. You are a genius.')
         break
     else:
-        Attempts -= 1
-        if Attempts != 0:
-            print(f'Your guess is wrong.Try again.(No of limits remained {Attempts})')
+        attempts -= 1
+        if attempts != 0:
+            print(f'Your guess is wrong. Try again.(No of limits remained = {attempts})')
 else:
     print('''Your guess is wrong and you have exceeded your limits.The secret no. was 7....''')
